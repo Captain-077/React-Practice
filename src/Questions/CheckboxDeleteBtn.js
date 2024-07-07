95648f38836689d01e84e63b57cc23d6d49e74fe
Your Name <nakuldesigner77@gmail.com>
2024-07-07 22:05:00 +0530

    delete button added on checked item with props and child comp

--- src/Questions/CheckboxDeleteBtn.js
+++ src/Questions/CheckboxDeleteBtn.js
@@ -1,33 +1,46 @@
 import React, { useState } from 'react'
+import List from './List';
 
 export default function CheckList() {
     const arr = ["play cricket", "Read a book", "Play video game"];
-    const [list, setList] = useState(arr)
-    console.log(list)
+    const [list, setList] = useState(arr);
+   
 
     const handleDelete = (target) => {
-        console.log(target)
-        const updatedlist = arr.filter((item, index) => {
+        console.log(target);
+
+        let newArr = list
+        const updatedlist = newArr.filter((item, index) => {
             if (item !== target) {
-                return item;
+                return item !== target
             }
         })
         console.log(updatedlist);
         setList(updatedlist);
-
     }
+
+    // const handlecheck = (i) => {
+
+    //   let boxarr = list
+    //     const checkupdate = boxarr.filter((item,index) => {
+    //         if (i == index){
+    //             setCheck(!check)
+    //         }
+          
+    //     })
+    //     // console.log(checkupdate)
+    //     // // setCheck(checkupdate)
+    // }
+
 
 
     return (
         <>
             <div className='flex flex-col items-center gap-10'> <h1>CheckList</h1>
                 {
-                    list.map((item) => {
+                    list.map((item,index) => {
                         return (
-                            <div className='flex justify-center items-center w-1/2 bg-blue-200 gap-10 rounded-full h-24'>
-                                <li><input className="form-check-input mx-2 p-5 inline-block w-5 scale-150" type="checkbox" value id="" />{item}</li>
-                                <button className='p-4 w-30 text-xl text-white bg-red-900 rounded-full' onClick={() => handleDelete(item)}>Delete</button>
-                            </div>
+                           <List item={item} index={index} handleDelete={handleDelete}/>
                         )
                     })
                 }
---
+++ src/Questions/List.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+
+export default function List({item,handleDelete}) {
+    const [check, setCheck] = useState(false);
+
+    return (
+        <div className='flex justify-center items-center w-1/2 bg-blue-200 gap-10 rounded-full h-24'>
+            <li><input className="form-check-input mx-2 p-5 inline-block w-5 scale-150" onChange={(e) => setCheck(e.target.checked)} type="checkbox" value id="" />{item}</li>
+            { check &&
+            <button className={`p-4 w-30 text-xl text-white bg-red-900 rounded-full`} onClick={() => handleDelete(item)}>Delete</button>}
+        </div>
+    )
+}
