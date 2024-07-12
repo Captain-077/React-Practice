1dae9e16ff176bb951c735c24c3676b14322478a
Your Name <nakuldesigner77@gmail.com>
2024-07-13 01:34:04 +0530

    ACP test todo working delete btn added with functionality

--- src/ACP test/Todo.js
+++ src/ACP test/Todo.js
@@ -11,17 +11,39 @@ export default function Todo() {
     }
 
     const handleClick = () => {
-        setTask(prev => [...prev,text]);
+        setTask(prev => [...prev, text]);
+        setText("");
     }
+
+    const handleDelete = (target) => {
+
+        let taskCopy = task
+
+      const updatedArr = taskCopy.filter((item, index) => {
+            return target !== index
+        })
+
+        setTask(updatedArr);
+
+    }
+
+
 
     return (
         <>
             <h1>Todo List Test</h1>
-            <input type="text" placeholder='enter your task' onChange={(e) => handlechange(e)} />
-            <button onClick={handleClick}>Add</button>
+            <input type="text" value={text} placeholder='enter your task' onChange={(e) => handlechange(e)} className='p-2 bg-grey-500 border-2' />
+            <button onClick={handleClick} className='p-2 bg-green-500 text-white'>Add</button>
             {
-                task.map((item,index) => {
-                   return <li key={index}>{item}</li>
+                task.map((item, index) => {
+                    return (
+                        <div className={`flex justify-center my-10`} key={index}>
+                            <li className='mx-5'>{item}</li>
+                            <button className='p-2 bg-red-500 text-white' onClick={() => handleDelete(index)}>Delete</button>
+                        </div>
+
+
+                    )
                 })
             }
 
